88b678af5b66da77fc878944e0be5b86d9c5ac65
Santhosh Kumar J <67225352+Sanfinity@users.noreply.github.com>
2025-02-03 07:18:01 +0530

    Update UnrolledLinkedList.cpp

--- UnrolledLinkedList.cpp
+++ UnrolledLinkedList.cpp
@@ -7,7 +7,7 @@ public:
     Node<T>* next;
     T data;
 
-    Node(int value) : next(nullptr), data(value) {}
+    Node(T inputData) : next(nullptr), data(inputData) {}
 };
 
 template<typename T>
@@ -16,6 +16,7 @@ public:
     Block<T>* next;
     Node<T>* head;
     int nodeCount;
+    static const int MAX_NODES = 3;
 
     Block() : next(nullptr), head(nullptr), nodeCount(0) {}
 };
@@ -25,7 +26,42 @@ class UnrolledLL {
 private:
     Block<T>* blockHead;
 
+    void shift(Block<T>* block) {
+        if (block->nodeCount > Block<T>::MAX_NODES) {
+            Node<T>* temp = block->head;
+            for (int i = 0; i < Block<T>::MAX_NODES / 2; i++) {
+                temp = temp->next;
+            }
+
+            Block<T>* newBlock = new Block<T>();
+            newBlock->next = block->next;
+            block->next = newBlock;
+            newBlock->head = temp->next;
+            temp->next = nullptr;
+
+            newBlock->nodeCount = block->nodeCount - (Block<T>::MAX_NODES / 2 + 1);
+            block->nodeCount = Block<T>::MAX_NODES / 2 + 1;
+        }
+    }
+
 public:
+    UnrolledLL() : blockHead(nullptr) {}
+
+    ~UnrolledLL() {
+        Block<T>* currentBlock = blockHead;
+        while (currentBlock) {
+            Node<T>* current = currentBlock->head;
+            while (current) {
+                Node<T>* next = current->next;
+                delete current;
+                current = next;
+            }
+            Block<T>* nextBlock = currentBlock->next;
+            delete currentBlock;
+            currentBlock = nextBlock;
+        }
+    }
+
     void display() {
         if (!blockHead) {
             cout << "Empty List!" << endl;
@@ -33,10 +69,10 @@ public:
         }
 
         Block<T>* temp = blockHead;
-        cout << "List Content: {";
+        cout << "List Content: {" << endl;
         while (temp) {
             Node<T>* tempNode = temp->head;
-            for (int i = 0; i < temp->nodeCount;i++) {
+            for (int i = 0; i < temp->nodeCount; i++) {
                 cout << tempNode->data << " ";
                 tempNode = tempNode->next;
             }
@@ -47,30 +83,50 @@ public:
     }
 
     void insert(T data, int position) {
-        Node<T>* newNode = new Node(data);
+        Node<T>* newNode = new Node<T>(data);
         if (!blockHead) {
             Block<T>* newBlock = new Block<T>();
             newBlock->head = newNode;
+            newBlock->nodeCount++;
             blockHead = newBlock;
         }
         else {
+            Block<T>* currentBlock = blockHead;
+            int currentPosition = 0;
+
+            while (currentBlock->next && currentPosition + currentBlock->nodeCount < position) {
+                currentPosition += currentBlock->nodeCount;
+                currentBlock = currentBlock->next;
+            }
+
             if (position == 0) {
-                newNode->next = blockHead->head;
-                blockHead->head = newNode;
-                blockHead->nodeCount++;
-                shift(blockHead);
+                newNode->next = currentBlock->head;
+                currentBlock->head = newNode;
+                currentBlock->nodeCount++;
             }
             else {
-                Block<T>* tempBlock = new Block<T>();
-                Node<T>* tempNode = temp->head;
-
-                searchElement(position, &tempBlock, &tempNode);
+                Node<T>* temp = currentBlock->head;
+                for (int i = 1; i < position - currentPosition; i++) {
+                    temp = temp->next;
+                }
+                newNode->next = temp->next;
+                temp->next = newNode;
+                currentBlock->nodeCount++;
             }
+
+            shift(currentBlock);
         }
     }
 };
 
-int main() {
+int ullmain() {
     cout << "### Program Started ###" << endl;
+    UnrolledLL<int> ull;
+    ull.insert(2, 0);
+    ull.insert(3, 1);
+    ull.insert(4, 2);
+    ull.insert(5, 3);
+    ull.insert(6, 4);
+    ull.display();
     return 0;
 }
