46d9b9881bde40d0468fad3e4b637c47604d61f7
Santhosh Kumar J <67225352+Sanfinity@users.noreply.github.com>
2025-05-07 23:18:17 +0530

    Create ArrayProblems1.cpp

---
+++ Array/ArrayProblems1.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+    cout << "\n###### START OF PROGRAM ######\n";
+    // Array => DS - similar elements
+    // Declaring inside main will have garbage values but when declared globally it will be filled with 0s
+    // 10^6 is the max size of Array inside main and 10^7 globally
+    int n = 5;
+    int arr[5] = { 1,2,2,6,4 };
+    // Largest element in an array = O(n)
+    int max = arr[0];
+    for (int i = 1; i < n;i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    cout << "Largest element is " << max << endl;
+    // Second Largest element
+    // we can sort and then find the send largest but that'll be like brute force = O(2n)
+    auto secLargest = -1;
+    for (int i = 0; i < n;i++) {
+        if (arr[i] > secLargest && arr[i] != max) {
+            secLargest = arr[i];
+        }
+    }
+    cout << "Second largest element is " << secLargest << endl;
+
+    // Optimal method = O(n)
+    max = arr[0];
+    secLargest = -1;
+    for (int i = 1; i < n;i++) {
+        if (arr[i] > max) {
+            secLargest = max;
+            max = arr[i];
+        }
+        else if (arr[i] > secLargest && arr[i] < max) {
+            secLargest = arr[i];
+        }
+    }
+    cout << "Largest element is " << max << endl;
+    cout << "Second largest element is " << secLargest << endl;
+
+    // Second smallest element in an array
+    vector<int> a = { 1,5,7,2,6,8,10 };
+    int smallest = a[0];
+    int secSmallest = INT_MAX;
+    for (int i = 1; i < a.size();i++) {
+        if (a[i] < smallest) {
+            secSmallest = smallest;
+            smallest = a[i];
+        }
+        else if (a[i] > smallest && a[i] < secSmallest) {
+            secSmallest = a[i];
+        }
+    }
+    cout << "Smallest element is " << smallest << endl;
+    cout << "Second Smallest element is " << secSmallest << endl;
+
+
+    cout << "\n###### END OF PROGRAM ######\n";
+}
