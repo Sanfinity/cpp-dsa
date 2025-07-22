261688ed4ab2f2ecd2982eaf1edc4269ee2cbf9b
Santhosh Kumar J <67225352+Sanfinity@users.noreply.github.com>
2025-07-22 21:36:16 +0530

    Update part1.cpp

--- DSA70/part1.cpp
+++ DSA70/part1.cpp
@@ -27,11 +27,43 @@ int missingNumber(const vector<int>& nums) {
     return expectedSum - actualSum;
 }
 
+// Given an array nums of length n where:
+// Each element is in the range [1, n]
+// Some elements appear twice, others once
+// Find all the numbers in the range [1, n] that do not appear in nums.
+// Time: O(n), Space: O(1) (excluding output)
+std::vector<int> findDisappearedNumbers(std::vector<int>& nums) {
+    for (int i = 0; i < nums.size(); i++) {
+        auto index = abs(nums[i]) - 1;
+        if (nums[index] > 0) {
+            nums[index] = -nums[index]; // Mark as visited by negation
+        }
+    }
+
+    std::vector<int> results;
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] > 0) {
+            results.emplace_back(i + 1);
+        }
+    }
+    return results;
+}
+
 int main()
 {
     //vector<int> nums = { 1, 2, 3, 4 };
-    vector<int> nums = {3, 0, 1};
     //cout << (containsDuplicate(nums) ? "True" : "False") << endl;
-    std::cout << "Missing number: " << missingNumber(nums) << std::endl;
+
+    //vector<int> nums = {3, 0, 1};
+    //std::cout << "Missing number: " << missingNumber(nums) << std::endl;
+
+    std::vector<int> nums = { 4,3,2,7,8,2,3,1 };
+    std::vector<int> result = findDisappearedNumbers(nums);
+
+    std::cout << "Missing numbers: ";
+    for (int num : result) {
+        std::cout << num << " ";
+    }
+
     return 0;
 }
